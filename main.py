8ca4e62c85e65a78a7ea1e192c645a8682e9fc7f
Joe Huang <happyjoe2008@gmail.com>
2020-01-29 01:13:24 -0800

    Use "groupyby" to speed up calculations

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import time
 import datetime
-from gaps import Gaps
+from more_itertools import pairwise
 
 # log start time
 start_time = time.time()
@@ -10,31 +10,21 @@ start_time = time.time()
 # load input data set (Python pickle file)
 df = pd.read_pickle('px.xz')
 
-# Dataframe needs to be sorted from earliest to latest
-sorted_df = df.sort_values('dt')
+grouped_df = df.groupby('bbgid', sort=False)['dt'].apply(list).reset_index(name='dates')
 
-last_seen = {}
-gaps = Gaps()
-
-for _, row in df.iterrows():
+def foo(row):
   bbgid = row['bbgid']
-  date = row['dt']
+  dates = row['dates']
+  dates.sort()
+  for old_date, new_date in pairwise(dates):
+    length = (new_date - old_date).days
+    gaps.append([old_date, new_date, length, bbgid])
 
-  # If we have seen this id before, we can compare to when it was last seen to see if there were any gaps
-  if bbgid in last_seen:
-    length = (date - last_seen[bbgid]).days
-
-    # We'd expect to see a gap of one day between consecutive records
-    if length > 1:
-
-      # It's cheaper to store all the data as a list of lists, and then convert into a dataframe
-      # Rather than create a dataframe and append row by row
-      gaps.add(start=last_seen[bbgid], end=date, length=length, bbgid=bbgid)
-    
-  last_seen[bbgid] = date
+gaps = []
+grouped_df.apply(foo, axis=1)  
 
 # export result to Excel
-stats = gaps.to_dataframe()
+stats = pd.DataFrame(gaps, columns=['start', 'end', 'length', 'bbgid']).sort_values('length', ascending=False)
 stats.iloc[0:1000].to_excel('px_stats.xlsx') 
 
 # show execution time
